e13034df93cec555ca4a327774a39e0dde446afa
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-16 16:51:36 +0100

    Update src/modelling/t5/parameters.py

--- src/modelling/t5/parameters.py
+++ src/modelling/t5/parameters.py
@@ -6,13 +6,26 @@ import transformers
 
 class Parameters(typing.NamedTuple):
     """
-    Class: Parameters
-
     For setting terms that are particular to a pre-trained model architecture type
+
+    Attributes
+    ----------
+    input_prefix: str
+
+    checkpoint: str
+
+    tokenizer: transformers.PreTrainedTokenizerFast
+
+    perturbation_interval: int
+        https://docs.ray.io/en/latest/tune/api/doc/ray.tune.schedulers.PopulationBasedTraining.html
+
+    n_trials: int
+
     """
 
     input_prefix: str = 'summarize: '
     checkpoint: str = 'google-t5/t5-small'
     tokenizer: transformers.PreTrainedTokenizerFast = (
         transformers.AutoTokenizer.from_pretrained(pretrained_model_name_or_path=checkpoint))
+    perturbation_interval: int = 2
     n_trials: int = 4
