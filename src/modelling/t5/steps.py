7da9e7bf9f2c444e23f257429ea451732db309f9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-16 16:51:58 +0100

    Update src/modelling/t5/steps.py

--- src/modelling/t5/steps.py
+++ src/modelling/t5/steps.py
@@ -27,13 +27,11 @@ class Steps:
         """
 
         self.__source = source
+        self.__device = device
 
         # A set of values for machine learning model development
-        self.__variable = vr.Variable()
-        self.__variable = self.__variable._replace(MODEL_OUTPUT_DIRECTORY=os.path.join(config.Config().warehouse, 't5'),
-            DEVICE=device, EPOCHS=2)
-
-        # Parameters
+        self.__n_epochs = 2
+        self.__variable = self.__get_variable()
         self.__parameters = pr.Parameters()
 
         # Logging
@@ -41,6 +39,24 @@ class Steps:
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
+
+    def __get_variable(self):
+        """
+
+        :return:
+        """
+
+        variable = vr.Variable()
+
+        # ... steps & epochs
+        max_steps_per_epoch = self.__source['train'].shape[0] // (self.__variable.TRAIN_BATCH_SIZE * self.__variable.N_GPU)
+        max_steps = max_steps_per_epoch * self.__n_epochs
+
+        # Update
+        variable = variable._replace(MODEL_OUTPUT_DIRECTORY=os.path.join(config.Config().warehouse, 't5'),
+                                     DEVICE=self.__device, EPOCHS=self.__n_epochs, MAX_STEPS=max_steps)
+
+        return variable
 
     def exc(self):
         """
@@ -53,8 +69,10 @@ class Steps:
         src.modelling.t5.depositories.Depositories().exc(path=self.__variable.MODEL_OUTPUT_DIRECTORY)
 
         # Temporary
+        self.__logger.info(self.__source['train'].shape)
         rays: dict[str, ray.data.dataset.MaterializedDataset] = src.modelling.t5.rays.Rays(
             source=self.__source, variable=self.__variable, parameters=self.__parameters).exc()
+        self.__logger.info(rays['train'].count())
 
         # Preprocessing Instance: For tokenization.  Converting each split into a T5 tokenized split
         # preprocessing = src.modelling.t5.preprocessing.Preprocessing(variable=self.__variable, parameters=self.parameters)
