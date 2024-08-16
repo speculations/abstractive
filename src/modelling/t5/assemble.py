00f0f9e168220e3b8a3c2cd30f7a8ea70fd0115c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-16 23:25:54 +0100

    Update src/modelling/t5/assemble.py: Under Development

--- src/modelling/t5/assemble.py
+++ src/modelling/t5/assemble.py
@@ -1,17 +1,15 @@
 """Module assemble.py"""
-import ray
+import logging
 import ray.data
-import ray.train.huggingface.transformers as rt
+
 import ray.train.torch
 import ray.tune
 import ray.tune.schedulers
-import transformers
 
 import src.elements.variable as vr
-import src.modelling.t5.intelligence
-import src.modelling.t5.metrics
 import src.modelling.t5.parameters as pr
 import src.modelling.t5.settings
+import src.modelling.t5.architecture
 
 
 class Assemble:
@@ -33,42 +31,29 @@ class Assemble:
         self.__variable = variable
         self.__parameters = parameters
 
+        # The trainer & train loop configuration
+        self.__arc = src.modelling.t5.architecture.Architecture()
+        self.__train_loop_config = {'variable': self.__variable, 'parameter': self.__parameters}
+
         # Settings
-        self.__metrics = src.modelling.t5.metrics.Metrics(parameters=self.__parameters)
         self.__settings = src.modelling.t5.settings.Settings(variable=self.__variable, parameters=self.__parameters)
-        self.__intelligence = src.modelling.t5.intelligence.Intelligence(variable=variable, parameters=parameters)
 
-    def __trainer(self):
-        """
-        https://huggingface.co/docs/transformers/main_classes/trainer#transformers.Seq2SeqTrainer
-
-        :return:
-        """
-
-        trainer = transformers.Seq2SeqTrainer(
-            model_init=self.__intelligence.model,
-            args=self.__settings.args(),
-            train_dataset=self.__intelligence.iterable(segment='train', batch_size=self.__variable.TRAIN_BATCH_SIZE),
-            eval_dataset=self.__intelligence.iterable(segment='eval', batch_size=self.__variable.VALIDATE_BATCH_SIZE),
-            tokenizer=self.__parameters.tokenizer,
-            data_collator=self.__intelligence.data_collator(),
-            compute_metrics=self.__metrics.exc
-        )
-        trainer.add_callback(rt.RayTrainReportCallback())
-        trainer = rt.prepare_trainer(trainer)
-        trainer.train()
-
-    def __trainable(self) -> ray.train.torch.TorchTrainer:
+    def exc(self):
         """
         https://docs.ray.io/en/latest/train/api/doc/ray.train.torch.TorchTrainer.html
         https://docs.ray.io/en/latest/train/api/doc/ray.train.ScalingConfig.html
         https://docs.ray.io/en/latest/train/api/doc/ray.train.CheckpointConfig.html
 
+        https://docs.ray.io/en/latest/tune/api/doc/ray.tune.TuneConfig.html
+        https://docs.ray.io/en/latest/train/api/doc/ray.train.RunConfig.html
+        https://docs.ray.io/en/latest/train/api/doc/ray.train.CheckpointConfig.html
+
         :return:
         """
 
-        return ray.train.torch.TorchTrainer(
-            self.__trainer,
+        trainable: ray.train.torch.TorchTrainer = ray.train.torch.TorchTrainer(
+            self.__arc.exc,
+            train_loop_config=self.__train_loop_config,
             scaling_config=ray.train.ScalingConfig(
                 num_workers=self.__variable.N_GPU,
                 use_gpu=True),
@@ -84,17 +69,8 @@ class Assemble:
             )
         )
 
-    def __call__(self):
-        """
-        https://docs.ray.io/en/latest/tune/api/doc/ray.tune.TuneConfig.html
-        https://docs.ray.io/en/latest/train/api/doc/ray.train.RunConfig.html
-        https://docs.ray.io/en/latest/train/api/doc/ray.train.CheckpointConfig.html
-
-        :return:
-        """
-
         tuner = ray.tune.Tuner(
-            trainable=self.__trainable,
+            trainable=trainable,
             param_space=self.__settings.param_space(),
             tune_config=ray.tune.TuneConfig(
                 scheduler=self.__settings.scheduler(),
@@ -105,9 +81,10 @@ class Assemble:
                 checkpoint_config=ray.train.CheckpointConfig(
                     num_to_keep=1,
                     checkpoint_score_attribute='eval_loss',
-                    checkpoint_score_order='min')
+                    checkpoint_score_order='min'),
+                progress_reporter=self.__settings.reporting()
             )
 
         )
 
-        return tuner.fit()
+        tuner.fit()
