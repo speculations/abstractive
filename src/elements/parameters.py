aea281754ad22a9497edb732be3368f54a24ed89
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-17 22:46:04 +0100

    Update src/elements/parameters.py

--- src/elements/parameters.py
+++ src/elements/parameters.py
@@ -1,28 +1,34 @@
 """Module parameters.py"""
-import typing
+import collections
 
 import transformers
 
 
-class Parameters(typing.NamedTuple):
+class Parameters():
     """
     For setting terms that are particular to a pre-trained model
     architecture type; T5 specific parameters.
 
-    Attributes
-    ----------
-    input_prefix: str
-
-    checkpoint: str
-
-    tokenizer: transformers.PreTrainedTokenizerFast
-
-    n_trials: int
-
     """
 
-    input_prefix: str = 'summarize: '
-    checkpoint: str = 'google-t5/t5-small'
-    tokenizer: transformers.PreTrainedTokenizerFast = (
-        transformers.AutoTokenizer.from_pretrained(pretrained_model_name_or_path=checkpoint))
-    n_trials: int = 4
+    name = 'google-t5/t5-small'
+
+    checkpoint: str
+    input_prefix: str
+    tokenizer: transformers.PreTrainedTokenizerFast
+    n_trials: int
+    Arguments = collections.namedtuple(
+        typename='Arguments',
+        field_names=['input_prefix', 'checkpoint', 'tokenizer', 'n_trials'])
+
+    arg = Arguments(
+        input_prefix='summarize', checkpoint=name,
+        tokenizer=transformers.AutoTokenizer.from_pretrained(pretrained_model_name_or_path=name), n_trials=4)
+
+
+
+    # input_prefix: str = 'summarize: '
+    # checkpoint: str = 'google-t5/t5-small'
+    # tokenizer: transformers.PreTrainedTokenizerFast = (
+    #     transformers.AutoTokenizer.from_pretrained(pretrained_model_name_or_path=checkpoint))
+    # n_trials: int = 4
