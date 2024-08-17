bf816f3007d38f3e8515e57c12409df23167c90b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-17 17:19:54 +0100

    Update src/modelling/t5/assemble.py

--- src/modelling/t5/assemble.py
+++ src/modelling/t5/assemble.py
@@ -6,7 +6,7 @@ import ray.train.torch
 import ray.tune
 import ray.tune.schedulers
 
-import src.elements.variable as vr
+import src.modelling.t5.custom
 import src.modelling.t5.parameters as pr
 import src.modelling.t5.settings
 import src.modelling.t5.architecture
@@ -17,23 +17,20 @@ class Assemble:
     Assemble
     """
 
-    def __init__(self, data: dict[str, ray.data.dataset.MaterializedDataset],
-                 variable: vr.Variable, parameters: pr.Parameters):
+    def __init__(self, data: dict[str, ray.data.dataset.MaterializedDataset]):
         """
 
         :param data: The project's data; parts train, validate, test
-        :param variable: A suite of values for machine learning
-                         model development
-        :param parameters: T5 specific parameters
         """
 
         self.__data = data
-        self.__variable = variable
-        self.__parameters = parameters
+        self.__variable = src.modelling.t5.custom.Custom().custom
+        self.__parameters = pr.Parameters()
 
         # The trainer & train loop configuration
         self.__arc = src.modelling.t5.architecture.Architecture()
-        self.__train_loop_config = {'variable': self.__variable, 'parameter': self.__parameters}
+        self.__train_loop_config = {'learning_rate': self.__variable.LEARNING_RATE, 'weight_decay': self.__variable.WEIGHT_DECAY,
+                                    'per_device_train_batch_size': self.__variable.TRAIN_BATCH_SIZE}
 
         # Settings
         self.__settings = src.modelling.t5.settings.Settings(variable=self.__variable, parameters=self.__parameters)
@@ -53,10 +50,10 @@ class Assemble:
 
         trainable: ray.train.torch.TorchTrainer = ray.train.torch.TorchTrainer(
             self.__arc.exc,
-            train_loop_config=self.__train_loop_config,
+            # train_loop_config=self.__train_loop_config,
             scaling_config=ray.train.ScalingConfig(
                 num_workers=self.__variable.N_GPU,
-                use_gpu=True),
+                use_gpu=True, trainer_resources={'CPU': self.__variable.N_CPU}),
             datasets={
                 'train': self.__data['train'],
                 'eval': self.__data['validate']
@@ -65,15 +62,16 @@ class Assemble:
 
         tuner = ray.tune.Tuner(
             trainable=trainable,
-            param_space=self.__settings.param_space(),
+            param_space=self.__train_loop_config,
             tune_config=ray.tune.TuneConfig(
                 scheduler=self.__settings.scheduler(),
-                num_samples=1
+                num_samples=2,
+                reuse_actors=True
             ),
             run_config=ray.train.RunConfig(
                 name='tuning',
                 checkpoint_config=ray.train.CheckpointConfig(
-                    num_to_keep=1,
+                    num_to_keep=5,
                     checkpoint_score_attribute='eval_loss',
                     checkpoint_score_order='min'),
                 progress_reporter=self.__settings.reporting()
