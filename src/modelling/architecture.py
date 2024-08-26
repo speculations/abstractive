7e59a55e4148b3c760842c82c073e68a80b5fd19
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-26 23:13:13 +0100

    Update src/modelling/architecture.py

--- src/modelling/architecture.py
+++ src/modelling/architecture.py
@@ -2,9 +2,12 @@
 import os
 
 import transformers
+import ray.train.huggingface.transformers as rt
 
 import src.elements.variable as vr
-
+import src.modelling.intelligence
+import src.modelling.metrics
+import src.modelling.parameters
 
 
 class Architecture:
@@ -13,26 +16,30 @@ class Architecture:
     Hyperparameters, etc.: learning rate, weight decay, the batch sizes, number of training epochs
     """
 
-    def __init__(self, max_steps: int):
+    def __init__(self):
         """
         Constructor
         """
 
-        variable = vr.Variable()
+    @staticmethod
+    def exc(config: dict):
 
-        self.args: transformers.Seq2SeqTrainingArguments = transformers.Seq2SeqTrainingArguments(
+        variable = vr.Variable()
+        parameters = src.modelling.parameters.Parameters().parameters
+
+        args: transformers.Seq2SeqTrainingArguments = transformers.Seq2SeqTrainingArguments(
             output_dir=variable.MODEL_OUTPUT_DIRECTORY,
             do_train=True,
             do_eval=True,
             eval_strategy='epoch',
             save_strategy='epoch',
             logging_strategy='epoch',
-            learning_rate=variable.LEARNING_RATE,
-            weight_decay=variable.WEIGHT_DECAY,
-            per_device_train_batch_size=variable.TRAIN_BATCH_SIZE,
+            learning_rate=config.get('learning_rate'),
+            weight_decay=config.get('weight_decay'),
+            per_device_train_batch_size=config.get('per_device_train_batch_size'),
             per_device_eval_batch_size=variable.VALIDATE_BATCH_SIZE,
             num_train_epochs=variable.EPOCHS,
-            max_steps=max_steps,
+            max_steps=config.get('max_steps'),
             warmup_steps=0,
             logging_dir=os.path.join(variable.MODEL_OUTPUT_DIRECTORY, '.logs'),
             no_cuda=False,
@@ -44,3 +51,21 @@ class Architecture:
             fp16=True,
             push_to_hub=False
         )
+
+        # Directives
+        metrics = src.modelling.metrics.Metrics(parameters=parameters)
+        intelligence = src.modelling.intelligence.Intelligence(parameters=parameters)
+
+        # Trainer
+        trainer = transformers.Seq2SeqTrainer(
+            model_init=intelligence.model, args=args,
+            train_dataset=intelligence.iterable(segment='train', batch_size=variable.TRAIN_BATCH_SIZE),
+            eval_dataset=intelligence.iterable(segment='eval', batch_size=variable.VALIDATE_BATCH_SIZE),
+            tokenizer=parameters.tokenizer,
+            data_collator=intelligence.collator(),
+            compute_metrics=metrics.exc
+        )
+        trainer.add_callback(rt.RayTrainReportCallback())
+        trainer = rt.prepare_trainer(trainer)
+
+        return trainer.train()
