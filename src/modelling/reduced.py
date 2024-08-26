80047789b9ba5aed84c4c2a0df74d70850756a97
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-27 00:07:03 +0100

    Update src/modelling/reduced.py

--- src/modelling/reduced.py
+++ src/modelling/reduced.py
@@ -44,25 +44,25 @@ class Reduced:
         arc = src.modelling.architecture.Architecture()
 
         # From Hugging Face Trainer -> Ray Trainer
-        trainable = ray.train.torch.TorchTrainer(train_loop_per_worker=arc.exc)
+        # trainable = ray.train.torch.TorchTrainer(train_loop_per_worker=arc.exc)
 
         tuner = ray.tune.Tuner(
-            trainable,
+            arc.exc,
             param_space={
                 "scaling_config": ray.train.ScalingConfig(
                     num_workers=self.__variable.N_GPU, use_gpu=True,
                     trainer_resources={'CPU': self.__variable.N_CPU}),
                 "datasets": {'train': self.__data['train'], 'eval': self.__data['validate']},
-                "train_loop_config": {'learning_rate': self.__variable.LEARNING_RATE,
-                                      'weight_decay': self.__variable.WEIGHT_DECAY,
-                                      'per_device_train_batch_size': self.__variable.TRAIN_BATCH_SIZE,
-                                      'max_steps': self.__numerics()}
+                "learning_rate": self.__variable.LEARNING_RATE,
+                "weight_decay": self.__variable.WEIGHT_DECAY,
+                "per_device_train_batch_size": self.__variable.TRAIN_BATCH_SIZE,
+                "max_steps": self.__numerics()
             },
             tune_config=ray.tune.TuneConfig(
                 metric='eval_loss', mode='min',
                 scheduler=self.__settings.scheduler(),
-                num_samples=1, reuse_actors=True,
-                search_alg=ray.tune.search.bayesopt.BayesOptSearch()
+                num_samples=1, reuse_actors=True
+                # search_alg=ray.tune.search.bayesopt.BayesOptSearch()
             ),
             run_config=ray.train.RunConfig(
                 name='tuning',
