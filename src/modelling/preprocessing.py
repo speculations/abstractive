21b60e32749758d0c348dbd277cae410cf0a54e3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-27 02:54:50 +0100

    Add src/modelling/preprocessing.py

---
+++ src/modelling/preprocessing.py
@@ -0,0 +1,94 @@
+"""Module preprocessing.py"""
+import logging
+import collections
+import datasets.formatting.formatting
+import transformers
+import torch
+import ray
+
+import src.elements.variable as vr
+
+class Preprocessing:
+    """
+    Class Preprocessing
+
+    This class preprocesses a data batch, e.g., splits for model
+    development, in line with T5 (Text-To-Text Transfer Transformer)
+    architecture expectations.
+    """
+
+    def __init__(self, parameters: collections.namedtuple(typename='ModelArchitectureParameters',
+                                                          field_names=['input_prefix', 'checkpoint', 'tokenizer'])):
+        """
+
+        :param parameters: T5 specific parameters
+        """
+
+        self.__variable = vr.Variable()
+
+        # The T5 specific parameters
+        self.__parameters = parameters
+        self.__tokenizer: transformers.PreTrainedTokenizerFast = self.__parameters.tokenizer
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
+    def batches(self, blob: datasets.formatting.formatting.LazyBatch) -> transformers.BatchEncoding:
+        """
+
+        :param blob: training or testing data batch
+        :return:
+        """
+
+        # Independent Variable
+        inputs = [self.__parameters.input_prefix + segment for segment in blob['text']]
+        structure: transformers.BatchEncoding = self.__tokenizer(
+            text=inputs, max_length=self.__variable.MAX_LENGTH_INPUT, truncation=True)
+
+        # Dependent Variable; targets has a dictionary structure, wherein the keys are <input_ids> & <attention_mask>
+        targets: transformers.BatchEncoding = self.__tokenizer(
+            text_target=blob['summary'], max_length=self.__variable.MAX_LENGTH_TARGET, truncation=True)
+        structure['labels']  = targets['input_ids']
+
+        return structure
+
+    def __tokenization(self, blob):
+        """
+        blob | datasets.formatting.formatting.LazyBatch
+
+        :param blob: training or testing data batch
+        :return:
+        """
+
+        # Input: A dictionary structure, wherein the keys are <input_ids> & <attention_mask>
+        entries = [self.__parameters.input_prefix + segment for segment in blob['text']]
+        inputs = self.__tokenizer(text=entries, max_length=self.__variable.MAX_LENGTH_INPUT,
+                                     truncation=True, padding='max_length')
+
+        # Targets: A dictionary structure, wherein the keys are <input_ids> & <attention_mask>
+        targets = self.__tokenizer(text_target=blob['summary'].tolist(), max_length=self.__variable.MAX_LENGTH_TARGET,
+                                   truncation=True, padding='max_length')
+
+        # Beware
+        structure = dict()
+        structure['input_ids']  = torch.LongTensor(inputs['input_ids'])
+        structure['attention_mask']  = torch.LongTensor(inputs['attention_mask'])
+        structure['labels']  = torch.LongTensor(targets['input_ids'])
+        self.__logger.info(structure)
+
+        return structure
+
+    def iterables(self, segment: str, batch_size: int):
+        """
+
+        :param segment:
+        :param batch_size:
+        :return:
+        """
+
+        part = ray.train.get_dataset_shard(segment)
+        return part.iter_torch_batches(
+            batch_size=batch_size, collate_fn=self.__tokenization)
