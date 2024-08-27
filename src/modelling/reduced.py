ef20c0cadd6558bbaf7926f2a84d40312e07f95e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-27 01:37:50 +0100

    Update src/modelling/reduced.py

--- src/modelling/reduced.py
+++ src/modelling/reduced.py
@@ -17,18 +17,12 @@ class Reduced:
     Assemble
     """
 
-    def __init__(self, data: dict[str, ray.data.dataset.MaterializedDataset]):
+    def __init__(self):
+        """
+        Constructor
         """
 
-        :param data: The project's data; parts train, validate, test
-        """
-
-        self.__data = data
         self.__variable = vr.Variable()
-
-        # Evaluate maximum steps
-        self.__numerics = src.modelling.numerics.Numerics(data=data, variable=self.__variable)
-        logging.info('max_steps: %s', self.__numerics())
 
         # Settings
         self.__settings = src.modelling.settings.Settings()
@@ -38,6 +32,10 @@ class Reduced:
         """
         https://huggingface.co/docs/transformers/main_classes/trainer#transformers.Seq2SeqTrainer
 
+        A note vis-à-vis TuneConfig.  Search algorithms, e.g.,
+            search_alg=ray.tune.search.bayesopt.BayesOptSearch()
+        cannot be used with PopulationBasedTraining schedulers.
+
         :return:
         """
 
@@ -46,23 +44,21 @@ class Reduced:
         # From Hugging Face Trainer -> Ray Trainer
         # trainable = ray.train.torch.TorchTrainer(train_loop_per_worker=arc.exc)
 
+        # Tuner
         tuner = ray.tune.Tuner(
             arc.exc,
             param_space={
                 "scaling_config": ray.train.ScalingConfig(
                     num_workers=self.__variable.N_GPU, use_gpu=True,
                     trainer_resources={'CPU': self.__variable.N_CPU}),
-                "datasets": {'train': self.__data['train'], 'eval': self.__data['validate']},
                 "learning_rate": self.__variable.LEARNING_RATE,
                 "weight_decay": self.__variable.WEIGHT_DECAY,
-                "per_device_train_batch_size": self.__variable.TRAIN_BATCH_SIZE,
-                "max_steps": self.__numerics()
+                "per_device_train_batch_size": self.__variable.TRAIN_BATCH_SIZE
             },
             tune_config=ray.tune.TuneConfig(
                 metric='eval_loss', mode='min',
                 scheduler=self.__settings.scheduler(),
                 num_samples=1, reuse_actors=True
-                # search_alg=ray.tune.search.bayesopt.BayesOptSearch()
             ),
             run_config=ray.train.RunConfig(
                 name='tuning',
