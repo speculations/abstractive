9f27cf6fa429984fad940e646a70fbde8f1f15d7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-02 23:35:40 +0100

    Update src/modelling/parameters.py

--- src/modelling/parameters.py
+++ src/modelling/parameters.py
@@ -1,39 +1,30 @@
 """Module parameters.py"""
-import collections
-
 import transformers
+
+import src.modelling.arguments
 
 
 class Parameters:
     """
-    For setting terms that are particular to a pre-trained model
-    architecture type; T5 specific parameters.
+    Rename
 
-    input_prefix: str = 'summarize: '
-    checkpoint: str = 'google-t5/t5-small'
-    tokenizer: transformers.PreTrainedTokenizerFast = (
-        transformers.AutoTokenizer.from_pretrained(pretrained_model_name_or_path=checkpoint))
+    This class sets up the model's tokenizer.
     """
 
     def __init__(self):
         """
         Constructor
-
-        input_prefix: str
-        tokenizer: transformers.PreTrainedTokenizerFast
         """
 
-        self.__checkpoint: str = 'google-t5/t5-small'
+        self.__arguments: src.modelling.arguments.Arguments = src.modelling.arguments.Arguments()
 
     def __call__(self):
+        """
 
-        ModelArchitectureParameters = collections.namedtuple(
-            typename='ModelArchitectureParameters',
-            field_names=['input_prefix', 'checkpoint', 'tokenizer'])
+        :return:
+            tokenizer: transformers.PreTrainedTokenizerFast
+        """
 
-        return ModelArchitectureParameters(
-            input_prefix='summarize',
-            checkpoint=self.__checkpoint,
-            tokenizer=transformers.AutoTokenizer.from_pretrained(
-                pretrained_model_name_or_path=self.__checkpoint)
-        )
+        return transformers.AutoTokenizer.from_pretrained(
+                pretrained_model_name_or_path=self.__arguments.checkpoint)
+
