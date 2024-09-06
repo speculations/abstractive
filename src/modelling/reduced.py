e2ba12fedfccf40582c26551722bcfadeacad471
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-09-06 15:44:38 +0100

    Update src/modelling/reduced.py: Re-writing

--- src/modelling/reduced.py
+++ src/modelling/reduced.py
@@ -1,4 +1,5 @@
 """Module assemble.py"""
+import os
 
 import ray.data
 import ray.train.torch
@@ -51,8 +52,8 @@ class Reduced:
             trainable,
             param_space={
                 "train_loop_config": {
-                    'learning_rate': ray.tune.grid_search([0.00002, 0.0002, 0.002, 0.02]),
-                    'weight_decay': ray.tune.grid_search([0.0, 0.1, 0.2]),
+                    'learning_rate': ray.tune.grid_search([0.0001, 0.0002]),
+                    'weight_decay': ray.tune.grid_search([0.1, 0.2]),
                     'max_steps_per_epoch': self.__max_steps_per_epoch},
                 "scaling_config": ray.train.ScalingConfig(
                     num_workers=self.__variable.N_GPU,
@@ -62,11 +63,12 @@ class Reduced:
             tune_config=ray.tune.TuneConfig(
                 metric='eval_loss',
                 mode='min',
-                scheduler=rts.ASHAScheduler(time_attr='training_iteration', max_t=20, grace_period=1),
+                scheduler=rts.ASHAScheduler(time_attr='training_iteration', max_t=100, grace_period=1),
                 num_samples=1, reuse_actors=True
             ),
             run_config=ray.train.RunConfig(
                 name='tuning',
+                storage_path=os.path.join(os.getcwd(), 'warehouse', 't5', 'calc'),
                 progress_reporter=ray.tune.CLIReporter(
                     parameter_columns=['learning_rate', 'weight_decay', 'max_steps_per_epoch'],
                     metric_columns=['eval_loss', 'rouge1', 'rouge2', 'rougeLsum', 'median']),
